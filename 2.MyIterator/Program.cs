440ff7ee861b5128d173104baeee087c8f8c7758
karimzade_k <karimzade_k@STHQ012D-01>
2026-06-28 15:57:15 +0400

    iterator completed

--- 2.MyIterator/Program.cs
+++ 2.MyIterator/Program.cs
@@ -6,14 +6,38 @@ public class Program
     {
         //IEnumerator
 
-        List<int> numbers = Enumerable.Range(5, 20).ToList();
+        List<int> numbers = Enumerable.Range(5, 10).ToList();
 
-        foreach (var item in numbers)
+        // foreach (var item in numbers)
+        // {
+        //     Console.WriteLine(item);
+        // }
+
+        // IEnumerator<int> iterator = numbers.GetEnumerator();
+        // 
+        // while (iterator.MoveNext())
+        // {
+        //     Console.WriteLine(iterator.Current);
+        // }
+        // 
+        // Console.WriteLine("Yeniden");
+        // 
+        // iterator.Reset();
+        // 
+        // while (iterator.MoveNext())
+        // {
+        //     Console.WriteLine(iterator.Current);
+        // }
+
+
+        IEnumerable<int> collection = Enumerable.Range(5, 10).ToList();
+
+        foreach (var item in collection)
         {
             Console.WriteLine(item);
         }
 
 
-        Console.WriteLine("Hello, World!");
+        // Console.WriteLine("Hello, World!");
     }
 }
