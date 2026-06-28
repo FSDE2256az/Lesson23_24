dc60f1ac9a7ab04c4d00156e943937ac67df8c66
karimzade_k <karimzade_k@STHQ012D-01>
2026-06-28 16:17:13 +0400

    generic completed

---
+++ 3.Generic/Models/GenericClass.cs
@@ -0,0 +1,32 @@
+﻿namespace _3.Generic.Models;
+
+public class GenericClass<T>
+{
+    public T Data { get; set; }
+    public string Name { get; set; }
+
+
+
+
+    public T Foo()
+    {
+        T temp = default!;
+
+        Console.WriteLine(temp);
+
+        return temp;
+    }
+
+    public T Foo<Tmethod>()
+    {
+        T temp = default!;
+        Tmethod tempMethod = default!;
+
+        Console.WriteLine(temp);
+        Console.WriteLine(tempMethod);
+
+        return temp;
+    }
+
+
+}
---
+++ 3.Generic/Program.cs
@@ -0,0 +1,37 @@
+﻿using _3.Generic.Models;
+
+namespace _3.Generic;
+
+interface IRequest { }
+interface IRequest<T> { }
+
+interface IRequest<T, TResult> { }
+
+
+class Base { }
+class Base<T> { }
+class Base<T1, T2> { }
+
+struct Point { }
+struct Point<T> { }
+
+delegate void SomeDelegate<T>();
+
+record MyRecord<T> { }
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var genericClass = new GenericClass<int>();
+
+        genericClass.Data = 42;
+
+        Console.WriteLine(genericClass.Data);
+
+        genericClass.Foo();
+
+        genericClass.Foo<string>();
+    }
+}
